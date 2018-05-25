46befc12acc6a6a964372f1f9e76bc39fc4bcf02
Wung <wungmathing@gmail.com>
2018-05-25 17:49:26 -0400

    Allow Heroku to set the port

--- src/index.js
+++ src/index.js
@@ -3,4 +3,6 @@ const app = express()
 
 app.get("/", (req, res) => res.send("Hello World!"))
 
-app.listen(3000, () => console.log("Example app listening on port 3000!"))
+app.listen(process.env.PORT || 8080, () =>
+  console.log("Example app listening on port 3000!")
+)
